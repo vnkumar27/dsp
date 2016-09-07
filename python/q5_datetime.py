1a293ac8870341a9200a6e61512b2e0e992eebd3
Veena Kumar <veenakumar@Veenas-MacBook-Pro.local>
2016-09-07 11:57:19 -0700

    Code for 5a datetime

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -4,10 +4,44 @@
 date_start = '01-02-2013'  
 date_stop = '07-28-2015'   
 
+*Answer : 937 days*
+
+*Code:*
+
+from datetime import date
+
+d0 = date(2013,01,02)
+d1 = date(2015,07,28)
+delta = d1 - d0
+print (delta.days)
+
 ####b)  
 date_start = '12312013'  
 date_stop = '05282015'  
 
+*Answers: 513 days*
+
+*Code:*
+
+from datetime import date
+
+d0 = date(2013,12,31)
+d1 = date(2015,05,28)
+delta = d1 - d0
+print (delta.days)
+
 ####c)  
 date_start = '15-Jan-1994'  
 date_stop = '14-Jul-2015'  
+
+*Answer: 7850 days*
+
+*Code:*
+
+from datetime import date
+
+d0 = date(1994,01,15)
+d1 = date(2015,07,14)
+delta = d1 - d0
+print (delta.days)
+
